f391b51bff0646373a6e884a088fcccf8f0417e6
luiz-cosme <158987446+luiz-cosme@users.noreply.github.com>
2025-03-25 13:18:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {   
+    return {
+        "name": "skillsMember",
+        "description": "A member of the skills team",
+        "skills": [
+            "JavaScript",
+            "Node.js",
+            "React"
+        ],
+        "experience": 5 // years
+    }
+   
+}
